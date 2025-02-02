a91291b20984a8d3c96341ab66b0d97217eb2138
Aurel-2 <aurel4E554C4C@proton.me>
2025-02-02 17:42:38 +0100

    Ajoutez des fichiers projet.

---
+++ Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace Bibliotheque.Models
+{
+    public class Book(int id, string title, string author, string genre, int publicationyear, bool available)
+    {
+        public int Id { get; set; } = id;
+        public string Title { get; set; } = title;
+        public string Author { get; set; } = author;
+        public string Genre { get; set; } = genre;
+        public int PublicationYear { get; set; } = publicationyear;
+        public bool Available { get; set; } = available;
+    }
+}
